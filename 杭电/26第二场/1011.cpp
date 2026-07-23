086bb8ea1ad3cedf5fb1f780b298c32f7e91cb06
ZFM-1208 <cuikales954@gmail.com>
2026-07-23 20:47:52 +0800

    7.23

--- 杭电/26第二场/1011.cpp
+++ 杭电/26第二场/1011.cpp
@@ -12,7 +12,19 @@ double pi = acos(-1);
 const int N = 1e6, mod = 1e9+7, inf = 1e18 + 5;
 
 void solve(){
-    
+    int n; cin >> n;
+    vii a(n+2);
+    rep(i,1,n) cin >> a[i];
+    int ans = 0;
+    vector<vii> dp(n+1,vii(2, inf));
+    // dp[i][0]: 前i项贡献，且i比i+1先删除
+    // dp[i][1]: 前i项贡献，且i+1比i先删除
+    dp[0][0] = 0;
+    for(int i = 1; i <= n; i++){
+        dp[i][0] = min(dp[i - 1][0] + a[i + 1], dp[i - 1][1] + max(a[i - 1], a[i + 1]));
+        dp[i][1] = min(dp[i - 1][0], dp[i - 1][1] + a[i - 1]);
+    }
+    cout << min(dp[n][0], dp[n][1]) << endl;
 }
 
 signed main(){
