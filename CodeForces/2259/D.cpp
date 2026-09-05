588541e7970b7d9f09ca342254d5d3d90f3238cc
ZFM-1208 <cuikales954@gmail.com>
2026-09-05 23:38:19 +0800

    9.5

--- CodeForces/2259/D.cpp
+++ CodeForces/2259/D.cpp
@@ -12,7 +12,41 @@ double pi = acos(-1);
 const int N = 1e6, mod = 1e9+7, inf = 1e18 + 5;
 
 void solve(){
-    
+    int n; cin >> n;
+    vii a(n+1);
+    vii cun;
+    for(int i = 1; i <= n; i++){
+        cin >> a[i];
+        if(a[i] == 0) cun.pb(i);
+    }
+    if(cun.size() == 0){
+        cout << "YES" << endl;
+        cout << "AB";
+        for(int i = 3; i <= n; i++){
+            cout << "C";
+        }
+        cout << endl;
+    }else if(cun.size() == 1){
+        cout << "NO" << endl;
+    }else if(cun.size() >= 2){
+        cout << "YES" << endl;
+        vii ans(n+1,0);
+        for(int i: cun){
+            ans[i] = 1;
+        }
+        ans[cun[0]] = 2;
+        for(int i = 1; i <= n; i++){
+            if(ans[i] == 0){
+                cout << "C";
+            }else if(ans[i] == 1){
+                cout << "A";
+            }else{
+                cout << "B";
+            }
+        }
+        cout << endl;
+    }
+
 }
 
 signed main(){
